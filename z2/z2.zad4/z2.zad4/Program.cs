d9d204998f82e9701ce5fe0ec20e336274f52c1e
a-czypek <angelika.czypek@icloud.com>
2024-04-07 00:13:04 +0200

    .

---
+++ z2/z2.zad4/z2.zad4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace z2.zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt s1 = new Punkt(6,9);
+            Punkt s2 = new Punkt(5,3);
+
+            string w = s1.Przesuniecie(s1,s2);
+            double dl = s1.Dlugosc(s1 ,s2);
+            bool s = s1.Porownaj(s1 ,s2);
+
+            Console.WriteLine($"Pierwszy punkt o współrzędnych: [{s1.X}, {s1.Y}].");
+            Console.WriteLine($"Drugi punkt o współrzędnych: [{s2.X}, {s2.Y}]");
+            Console.WriteLine($"Przesunięcie punktu s1 o wektor [5,3]: {w}");
+            Console.WriteLine($"Długość pomiędzy tymi punktami wynosi: {dl}");
+            Console.WriteLine($"Czy punkty są równe? {s}");
+
+        }
+    }
+}
---
+++ z2/z2.zad4/z2.zad4/Punkt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2.zad4
+{
+    internal class Punkt
+    {
+        int x;
+        int y;
+
+        public Punkt()
+        {
+            x = 1;
+            y = 1;
+        }
+
+        public Punkt(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X
+        { 
+            get { return x; } 
+            set { x = value; }
+        
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public string Przesuniecie(Punkt p1, Punkt p2)
+        {
+            return $"Wektor: [{p1.x + p2.x}, {p1.y + p2.y}]";
+        }
+
+        public double Dlugosc(Punkt p1, Punkt p2)
+        {
+            double d = Math.Sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+            return d;
+        }
+
+        public bool Porownaj(Punkt p1, Punkt p2)
+        {
+            return p1.x == p2.x && p1.y == p2.y;
+        }
+    }
+}
