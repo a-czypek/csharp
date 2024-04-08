7e115edfbe3224848ccc0fd9aeb037e7c6c35f91
a-czypek <angelika.czypek@icloud.com>
2024-04-08 19:41:51 +0200

    .

---
+++ z5/z5.zad2/z4.zad2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace z5.zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punkt a = new Punkt(3, 5);
+            Punkt b = new Punkt(3, 5);
+            Punkt e = new Punkt(3, 7);
+            Punkt c = a * 3;
+            Punkt d = 4 * a;
+
+            Console.WriteLine($"Punkt a: {a.x},{a.y}");
+            Console.WriteLine($"Punkt b: {b.x},{b.y}");
+
+            Console.WriteLine($"Punkt c: {c.x},{c.y}");
+            Console.WriteLine($"Punkt d: {d.x},{d.y}");
+
+            Console.WriteLine("Punkt p1 i p2 są równe: {0}", a == b);
+            Console.WriteLine("Punkt p1 i p2 są równe: {0}", b == e);
+        }
+    }
+}
---
+++ z5/z5.zad2/z4.zad2/Punkt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z5.zad2
+{
+    internal class Punkt
+    {
+        public float x {  get; set; }
+        public float y { get; set; }
+
+        public Punkt(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static Punkt operator *(int x, Punkt p)
+        {
+            return new Punkt(x * p.x, x * p.y);
+        }
+        public static Punkt operator *(Punkt p, int x)
+        {
+            return new Punkt(p.x * x, p.y * x);
+        }
+        public static bool operator ==(Punkt p, Punkt a)
+        {
+            if (p.x == a.x && p.y == a.y)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Punkt p, Punkt a)
+        {
+            return !(p == a);
+        }
+    }
+}
